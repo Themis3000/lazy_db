8bc0c4ad2ea3eac5182fcc3aafa6400539d1f6cd
Themi <Tcm4760@gmail.com>
2021-12-20 05:49:44 -0600

    Tests working properly

--- tests/test_lazy_db.py
+++ tests/test_lazy_db.py
@@ -20,41 +20,63 @@ class TestLazy_db(unittest.TestCase):
         self.db.close()
         os.remove("test_db.lazydb")
 
-    def test_1_string_insertion(self):
-        """Test insertion"""
-        self.db.write("test_str", "here is a test value")
-        value = self.db.read("test_str")
-        self.assertEqual("here is a test value", value)
-
-    def test_1_string_insertion2(self):
-        """Test insertion"""
-        self.db.write("test_str2", "here is a test value2")
-
-    def test_2_string_read(self):
-        """Test string read"""
-        value = self.db.read("test_str")
-        self.assertEqual("here is a test value", value)
-
-    def test_2_string_read2(self):
-        """Test string read"""
-        value = self.db.read("test_str")
-        self.assertEqual("here is a test value2", value)
-
-    def test_3_close_db(self):
-        """Close database"""
+    def restart(self):
         self.db.close()
-
-    def test_4_open_db(self):
-        """Reopen database"""
         self.db = lazy_db.LazyDb("test_db.lazydb")
 
-    def test_5_second_test_string_read(self):
-        """Test string read after database is reopened"""
-        value = self.db.read("test_str")
-        self.assertEqual("here is a test value", value)
+    def test_string_insertions(self):
+        """Test 2 string insertions"""
+        self.db.write("test_str", "here is a test value")
+        self.db.write("test_str2", "here is a test value2")
+        value1 = self.db.read("test_str")
+        value2 = self.db.read("test_str2")
+        self.assertEqual("here is a test value", value1)
+        self.assertEqual("here is a test value2", value2)
 
-    def test_5_second_test_string_read2(self):
-        """Test string read after database is reopened"""
-        value = self.db.read("test_str2")
-        self.assertEqual("here is a test value2", value)
+    def test_int_insertions(self):
+        """test 2 int insertions"""
+        self.db.write("test_str", 346735)
+        self.db.write("test_str2", 982745)
+        value1 = self.db.read("test_str")
+        value2 = self.db.read("test_str2")
+        self.assertEqual(346735, value1)
+        self.assertEqual(982745, value2)
 
+    def test_int_key_insertions(self):
+        """test 2 int key insertions"""
+        self.db.write(43556, 346735)
+        self.db.write(234565, "test string")
+        value1 = self.db.read(43556)
+        value2 = self.db.read(234565)
+        self.assertEqual(346735, value1)
+        self.assertEqual("test string", value2)
+
+    def test_string_insertions_restart(self):
+        """Test 2 string insertions with restart"""
+        self.db.write("test_str", "here is a test value")
+        self.db.write("test_str2", "here is a test value2")
+        self.restart()
+        value1 = self.db.read("test_str")
+        value2 = self.db.read("test_str2")
+        self.assertEqual("here is a test value", value1)
+        self.assertEqual("here is a test value2", value2)
+
+    def test_int_insertions_restart(self):
+        """test 2 int insertions with restart"""
+        self.db.write("test_str", 346735)
+        self.db.write("test_str2", 982745)
+        self.restart()
+        value1 = self.db.read("test_str")
+        value2 = self.db.read("test_str2")
+        self.assertEqual(346735, value1)
+        self.assertEqual(982745, value2)
+
+    def test_int_key_insertions_restart(self):
+        """test 2 int key insertions with restart"""
+        self.db.write(43556, 346735)
+        self.db.write(234565, "test string")
+        self.restart()
+        value1 = self.db.read(43556)
+        value2 = self.db.read(234565)
+        self.assertEqual(346735, value1)
+        self.assertEqual("test string", value2)
