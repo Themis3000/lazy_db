7c1901d993b91676b00eb948b92de0219604b96b
Themi <Tcm4760@gmail.com>
2021-12-28 01:02:45 -0600

    Bump version: 0.1.0 → 0.1.1

--- lazy_db/__init__.py
+++ lazy_db/__init__.py
@@ -3,4 +3,4 @@ from .lazy_db import LazyDb
 
 __author__ = """Themi Megas"""
 __email__ = 'tcm4760@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Themis3000/lazy_db',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
