368c6b93f3dd5079c6c6e6456e5be22bb423a758
Themi <Tcm4760@gmail.com>
2021-12-28 19:44:08 -0600

    Bump version: 0.1.1 → 1.0.0

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Themis3000/lazy_db',
-    version='0.1.1',
+    version='1.0.0',
     zip_safe=False,
 )
